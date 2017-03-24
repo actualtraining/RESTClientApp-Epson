b23716e24c2fb3a45f43388fa752c589b0687a24
erick <erick@DESKTOP-JIKBK0G>
2017-03-24 11:06:44 +0700

    Add project files.

---
+++ RESTClientApp/RESTClientApp/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTClientApp
+{
+    public class Helpers
+    {
+        public static string GetUrl()
+        {
+            return "http://peb.azurewebsites.net/";
+        }
+    }
+}
---
+++ RESTClientApp/RESTClientApp/Services/KategoriServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp.Portable;
+using RestSharp.Portable.HttpClient;
+using RESTClientApp.Model;
+
+namespace RESTClientApp.Services
+{
+    public class KategoriServices
+    {
+        private RestClient _client;
+        public KategoriServices()
+        {
+            _client = new RestClient(Helpers.GetUrl());
+        }
+
+        public async Task<IEnumerable<Kategori>> GetAllKategori()
+        {
+            RestRequest request = new RestRequest("api/Kategori", Method.GET);
+             
+        }
+
+    }
+}
